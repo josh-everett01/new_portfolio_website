33434e574a2f03711d446f3529f94a423987bec3
Joshua Everett <59103702+josh-everett01@users.noreply.github.com>
2020-08-19 20:19:26 -0500

    Add projects section to portfolio site
    This commit will add the projects section to the portfolio website. The CSS will not be 100% responsive as of this commit. There will be one project card added so far. In the future there will still need to be styling changes as far as the text and animations go for this section. Possibly will need to add smooth scrolling feature for page transitions.

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ button.addEventListener('click', showContactInfo);
 function showContactInfo() {
   document.getElementById('flex-container').style.display = "none";
   document.getElementById('card-container').style.display = "none";
-document.getElementById('hidden-contact').style.display = "inline";
+  document.getElementById('hidden-contact').style.display = "inline";
 }
